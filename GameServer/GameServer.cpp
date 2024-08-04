f9c81840db37432c434882125cb742cc673d7e04
doge <synm4@naver.com>
2024-08-04 20:03:09 +0900

    RW 스핀락을 구현하여, 여러 쓰레드가 동시에 읽기를 수행할 수 있도록 하고, 쓰기 작업은 상호 배타적으로 수행되도록 함

---
+++ GameServer/GameServer.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "CorePch.h"
+#include <atomic>
+#include <mutex>
+#include <windows.h>
+#include <future>
+#include "ThreadManager.h"
+
+CoreGlobal Core;
+
+void ThreadMain()
+{
+	while (true)
+	{
+		cout << "Hello ! I am thread... " << LThreadId << endl;
+		this_thread::sleep_for(1s);
+	}
+}
+
+int main()
+{
+	for (int32 i = 0; i < 5; i++)
+	{
+		GThreadManager->Launch(ThreadMain);
+	}
+
+	GThreadManager->Join();
+}

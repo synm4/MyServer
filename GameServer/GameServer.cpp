86c60d0b0fc4b810d867ab0b221cacb0dfeec968
doge <synm4@naver.com>
2024-08-05 14:00:44 +0900

    데드락 탐지

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -7,73 +7,30 @@
 #include <future>
 #include "ThreadManager.h"
 
-class TestLock
-{
-	USE_LOCK;
-
-public:
-	int32 TestRead()
-	{
-		READ_LOCK;
-
-		if (_queue.empty())
-			return -1;
-
-		return _queue.front();
-	}
-
-	void TestPush()
-	{
-		WRITE_LOCK;
-
-		_queue.push(rand() % 100);
-	}
-
-	void TestPop()
-	{
-		WRITE_LOCK;
-
-		if (_queue.empty() == false)
-			_queue.pop();
-	}
-
-private:
-	queue<int32> _queue;
-};
-
-TestLock testLock;
-
-void ThreadWrite()
-{
-	while (true)
-	{
-		testLock.TestPush();
-		this_thread::sleep_for(1ms);
-		testLock.TestPop();
-	}
-}
-
-void ThreadRead()
-{
-	while (true)
-	{
-		int32 value = testLock.TestRead();
-		cout << value << endl;
-		this_thread::sleep_for(1ms);
-	}
-}
+#include "AccountManager.h"
+#include "PlayerManager.h"
 
 int main()
 {
-	for (int32 i = 0; i < 2; i++)
+	GThreadManager->Launch([=]
 	{
-		GThreadManager->Launch(ThreadWrite);
-	}
-
-	for (int32 i = 0; i < 5; i++)
+		while (true)
+		{
+			cout << "PlayerThenAccount" << endl;
+			GPlayerManager.PlayerThenAccount();
+			this_thread::sleep_for(100ms);
+		}
+	});
+	// debug 모드 실행시 데드락을 바로 탐지할수있음.
+	GThreadManager->Launch([=]
 	{
-		GThreadManager->Launch(ThreadRead);
-	}
+		while (true)
+		{
+			cout << "AccountThenPlayer" << endl;
+			GAccountManager.AccountThenPlayer();
+			this_thread::sleep_for(100ms);
+		}
+	});
 
 	GThreadManager->Join();
 }

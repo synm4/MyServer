3f77c6041004f23814c1fb520a831cd221bdb39a
doge <synm4@naver.com>
2024-08-06 10:01:06 +0900

    Reference Counting 추가

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -7,30 +7,83 @@
 #include <future>
 #include "ThreadManager.h"
 
-#include "AccountManager.h"
-#include "PlayerManager.h"
+#include "RefCounting.h"
+
+class Wraight : public RefCountable
+{
+public:
+	int _hp = 150;
+	int _posX = 0;
+	int _posY = 0;
+};
+
+using WraightRef = TSharedPtr<Wraight>;
+
+class Missile : public RefCountable
+{
+public:
+	void SetTarget(WraightRef target)
+	{
+		_target = target;
+		// 중간에 개입 가능
+		//target->AddRef();
+	}
+
+	bool Update()
+	{
+		if (_target == nullptr)
+			return true;
+
+		int posX = _target->_posX;
+		int posY = _target->_posY;
+
+		// TODO : 쫓아간다
+
+		if (_target->_hp == 0)
+		{
+			//_target->ReleaseRef();
+			_target = nullptr;
+			return true;
+		}
+
+		return false;
+	}
+
+	WraightRef _target = nullptr;
+};
+
+using MissileRef = TSharedPtr<Missile>;
 
 int main()
 {
-	GThreadManager->Launch([=]
-	{
-		while (true)
-		{
-			cout << "PlayerThenAccount" << endl;
-			GPlayerManager.PlayerThenAccount();
-			this_thread::sleep_for(100ms);
-		}
-	});
-	// debug 모드 실행시 데드락을 바로 탐지할수있음.
-	GThreadManager->Launch([=]
-	{
-		while (true)
-		{
-			cout << "AccountThenPlayer" << endl;
-			GAccountManager.AccountThenPlayer();
-			this_thread::sleep_for(100ms);
-		}
-	});
+// 중단점을 잡고 wraight or missile 의 ->_ptr->refcount 를 보면 
+// 릴리즈하거나 생성자호출시 참조 카운트를 더하고 빼는걸 확인할 수 있음
+	WraightRef wraight(new Wraight()); 
+	wraight->ReleaseRef();
+	MissileRef missile(new Missile());
+	missile->ReleaseRef();
 
-	GThreadManager->Join();
+	missile->SetTarget(wraight);
+
+	// 레이스가 피격 당함
+	wraight->_hp = 0;
+	//delete wraight;
+	//wraight->ReleaseRef();
+	wraight = nullptr;
+
+	while (true)
+	{
+		if (missile)
+		{
+			if (missile->Update())
+			{
+				//missile->ReleaseRef();
+				missile = nullptr;
+			}
+		}
+	}
+
+	//missile->ReleaseRef();
+	missile = nullptr;
+	//delete missile;
 }

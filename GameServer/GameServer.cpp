bd589246398d367a8fabb0f14a633133f2a3ad02
doge <synm4@naver.com>
2024-08-09 20:09:16 +0900

    원하는 방식의 allocate 방식으로 stl 컨테이너를 사용

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -9,6 +9,7 @@
 
 #include "RefCounting.h"
 #include "Memory.h"
+#include "Allocator.h"
 
 class Player
 {
@@ -41,22 +42,12 @@ public:
 
 int main()
 {
-	// new 와 delete 에서는 무조건 메모리를 날리지않고 힙영역을 유동적으로 관리를함. 
-	// 해제한 메모리 접근시 크래시가 날 수 도있고 안날수도있음.
-	// winapi 인 VirtualAlloc 은 운영체제에게 아예 해당 메모리를 해제해달라고 요청하기때문에 
-	// 해제한 메모리 접근시 바로 크래시가남.
-	
-	// CoreMacro.h 에서 BaseAllocator로 바꿔보면 메모리 오염에대한 크래시를 잡지 못하는것을 알 수 있음.
-	
-	// 기존 stompallocator 에서 4kb로 페이징을 하고있는데, 할당한 영역에 비해
-	// 실제 사용되는 영역이 터무니없이 작기 때문에 초과되는 영역의 접근을 막을 수 없음.
-	// 오버플로우 문제를 막기위해 메모리 할당영역의 끝단으로 생성하도록 변경하여 해결. (언더플로우는 사실 없다고 봐도 무방)
-	Knight* knight = (Knight*)xnew<Player>();
+	// [                    [   ]]
+	// 원하는 방식의 allocate 방식으로 stl 컨테이너를 사용
+	// Container에 template와 using 으로 정의됨
+	Vector<Knight> v(100);
 
-	knight->_hp = 100;
-
-	xdelete(knight);
-
-	
+	Map<int32, Knight> m;
+	m[100] = Knight();
 
 }
--- ServerCore/CorePch.h
+++ ServerCore/CorePch.h
@@ -4,15 +4,8 @@
 #include "CoreMacro.h"
 #include "CoreTLS.h"
 #include "CoreGlobal.h"
+#include "Container.h"
 
-#include <vector>
-#include <list>
-#include <queue>
-#include <stack>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
 
 #include <Windows.h>
 #include <iostream>

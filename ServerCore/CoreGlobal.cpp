8b1bd91b6fc49c66a9b8d0e70f429bb8e839f2a0
doge <synm4@naver.com>
2024-09-13 09:56:39 +0900

    RecvBuffer

---
+++ ServerCore/CoreGlobal.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "CoreGlobal.h"
+#include "ThreadManager.h"
+#include "Memory.h"
+#include "DeadLockProfiler.h"
+#include "SocketUtils.h"
+
+ThreadManager*		GThreadManager = nullptr;
+Memory*				GMemory = nullptr;
+DeadLockProfiler*	GDeadLockProfiler = nullptr;
+
+class CoreGlobal
+{
+public:
+	CoreGlobal()
+	{
+		GThreadManager = new ThreadManager();
+		GMemory = new Memory();
+		GDeadLockProfiler = new DeadLockProfiler();
+		SocketUtils::Init();
+	}
+
+	~CoreGlobal()
+	{
+		delete GThreadManager;
+		delete GMemory;
+		delete GDeadLockProfiler;
+		SocketUtils::Clear();
+	}
+} GCoreGlobal;

a3d8b2edbfa4d3915b1f5410e54e2b4525749127
doge <synm4@naver.com>
2024-09-20 11:18:00 +0900

    BufferHelpers

--- ServerCore/SendBuffer.h
+++ ServerCore/SendBuffer.h
@@ -10,11 +10,12 @@ class SendBuffer
 {
 public:
 
-	SendBuffer(SendBufferChunkRef owner, BYTE* buffer , int32 allocSize);
+	SendBuffer(SendBufferChunkRef owner, BYTE* buffer , uint32 allocSize);
 	~SendBuffer();
 
 	BYTE* Buffer() { return _buffer; }
-	int32 WriteSize() { return _writeSize; }
+	uint32 AllocSize() { return _allocSize; }
+	uint32 WriteSize() { return _writeSize; }
 	void Close(uint32 writeSize);
 
 private:
